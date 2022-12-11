7b413d3424ca4004d90345d5138db49021773beb
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-12-11 12:51:20 +0100

    config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { UiPageComponent } from './pages/ui-page/ui-page.component';
 
 const routes: Routes = [
   { path: 'ui', component: UiPageComponent },
+  { path: '', component: HomePageComponent },
   { path: '**', component: NotFoundPageComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { UiPageComponent } from './pages/ui-page/ui-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent, UiPageComponent, NotFoundPageComponent],
+  declarations: [
+    AppComponent,
+    UiPageComponent,
+    NotFoundPageComponent,
+    HomePageComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
