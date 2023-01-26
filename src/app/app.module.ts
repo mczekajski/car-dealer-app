8068e2bf1f7dba8136adf0e05e8bb81ab5e95af9
Marcin Czekajski <czekajskimarcin@gmail.com>
2023-01-26 20:45:31 +0100

    feat/homepage hero image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,14 @@ import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.com
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { UiPageModule } from './pages/ui-page/ui-page.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { BenefitsComponent } from './components/benefits/benefits.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NotFoundPageComponent, HomePageComponent],
+  declarations: [AppComponent, NotFoundPageComponent, HomePageComponent, HeaderComponent, CatalogComponent, BenefitsComponent, ContactComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
